04b632df3abb6fe164d089efc82eca07db55a1bb
Meaghan Campbell <emeaghancampbell@gmail.com>
2023-09-18 19:34:52 -0400

    export axios

--- frontend/axiosConfig.js
+++ frontend/axiosConfig.js
@@ -4,4 +4,9 @@ const baseURL = process.env.NODE_ENV === 'production'
     ? 'https://souschefapp-backend-560b3e209edf.herokuapp.com' 
     : 'http://localhost:3001';
 
-axios.defaults.baseURL = baseURL;
+
+const instance = axios.create({
+    baseURL: baseURL
+});
+    
+export default instance;
